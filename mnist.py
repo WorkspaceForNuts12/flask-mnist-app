ca050bf92d88fa7f3fade3db9ae796ac02ca5256
WorkspaceForNuts12 <153623778+WorkspaceForNuts12@users.noreply.github.com>
2024-02-17 18:44:03 +0900

    first commit

---
+++ mnist.py
@@ -0,0 +1,69 @@
+import os
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing import image
+
+import numpy as np
+
+
+classes = ["0","1","2","3","4","5","6","7","8","9"]
+image_size = 28
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./model.h5')#学習済みモデルをロード
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+
+            # redirect で元のページに戻る
+            return redirect(request.url)
+        
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+
+            # redirect で元のページに戻る            
+            return redirect(request.url)
+        
+
+        if file and allowed_file(file.filename):
+
+            # ファイル名のサニタイズ（危険な文字列が含まれていないかチェック）
+            filename = secure_filename(file.filename)
+
+             # ファイルの格納           
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            #受け取った画像をモノクロ、サイズ指定で読み込み、np形式に変換
+            img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
+            img = image.img_to_array(img)
+            data = np.array([img])
+            #変換したデータをモデルに渡して予測する
+            result = model.predict(data)[0]
+            predicted = result.argmax()
+            pred_answer = "これは " + classes[predicted] + " です"
+
+            # render_templateの引数にanswer=pred_answerと渡すことで、
+            # index.htmlに書いたanswerにpred_answerを代入することができる
+            return render_template("index.html",answer=pred_answer)
+
+    return render_template("index.html",answer="")
+
+
+if __name__ == "__main__":
+    app.run()
